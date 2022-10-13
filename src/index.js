a929a472b15f1bd0deb1bb2d21bc068079060f92
dpdurczok <dpdurczok@gmail.com>
2022-10-13 15:35:12 +0200

    https fix

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import './index.css';
-import { HashRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <React.StrictMode>
-        <HashRouter>
-        <App />
-        </HashRouter>
-    </React.StrictMode>
-
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
 )
