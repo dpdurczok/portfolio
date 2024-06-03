8f65ccd81d5dc8da4b0444d2d2986672c859cfee
dpdurczok <dpdurczok@gmail.com>
2024-06-03 17:47:17 +0200

    Automation

--- src/containers-chains/modelsChains/ModelsChains.jsx
+++ src/containers-chains/modelsChains/ModelsChains.jsx
@@ -1,7 +1,8 @@
 import React from "react"
 import "./modelsChains.css"
 import {Article} from "../../components"
-import { img01, img02, img03, img04, img05, img06, img07, img08, img09, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24, img25, img26 } from "./imports"
+
+import images from '../../assets/Chains/chainsModels'; // Import the images array
 
 const ModelsChains = () => {
    return(
@@ -14,32 +15,34 @@ const ModelsChains = () => {
         </p>
       </div>
       <div className="prt__modelsChains-container">
-          <Article imgUrl={img01} link="/" title="Farm House" paragraph=""/>
-          <Article imgUrl={img02} link="/" title="BlackSmith" paragraph=""/>
-          <Article imgUrl={img03} link="/" title="Weapons Shop" paragraph=""/>
-          <Article imgUrl={img04} link="/" title="Government Building" paragraph=""/>
-          <Article imgUrl={img05} link="/" title="Simple House" paragraph=""/>
-          <Article imgUrl={img06} link="/" title="Tall House" paragraph=""/>
-          <Article imgUrl={img07} link="/" title="Slide" paragraph=""/>
-          <Article imgUrl={img08} link="/" title="Hay Bale" paragraph=""/>
-          <Article imgUrl={img09} link="/" title="Crates" paragraph=""/>
-          <Article imgUrl={img10} link="/" title="Village Island" paragraph=""/>
-          <Article imgUrl={img11} link="/" title="Dead God Island" paragraph=""/>
-          <Article imgUrl={img12} link="/" title="Lobue" paragraph=""/>
-          <Article imgUrl={img13} link="/" title="Old lady" paragraph=""/>
-          <Article imgUrl={img14} link="/" title="Middle aged man" paragraph=""/>
-          <Article imgUrl={img15} link="/" title="Child" paragraph=""/>
-          <Article imgUrl={img16} link="/" title="Bob" paragraph=""/>
-          <Article imgUrl={img17} link="/" title="Ship" paragraph=""/>
-          <Article imgUrl={img18} link="/" title="Raft" paragraph=""/>
-          <Article imgUrl={img19} link="/" title="Stone Modular Components" paragraph=""/>
-          <Article imgUrl={img20} link="/" title="Wooden Modular Components" paragraph=""/>
-          <Article imgUrl={img21} link="/" title="Wooden Modular Components" paragraph=""/>
-          <Article imgUrl={img22} link="/" title="Palm tree" paragraph=""/>
-          <Article imgUrl={img23} link="/" title="Big Tree" paragraph=""/>
-          <Article imgUrl={img24} link="/" title="Oak Trees" paragraph=""/>
-          <Article imgUrl={img25} link="/" title="Red Mushrooms" paragraph=""/>
-          <Article imgUrl={img26} link="/" title="Brown Mushrooms" paragraph=""/>
+        <Article imgUrl={images[0]} link="/" title="Farm House" paragraph=""/>
+        <Article imgUrl={images[1]} link="/" title="BlackSmith" paragraph=""/>
+        <Article imgUrl={images[2]} link="/" title="Weapons Shop" paragraph=""/>
+        <Article imgUrl={images[3]} link="/" title="Government Building" paragraph=""/>
+        <Article imgUrl={images[4]} link="/" title="Simple House" paragraph=""/>
+        <Article imgUrl={images[5]} link="/" title="Tall House" paragraph=""/>
+        <Article imgUrl={images[6]} link="/" title="Slide" paragraph=""/>
+        <Article imgUrl={images[7]} link="/" title="Hay Bale" paragraph=""/>
+        <Article imgUrl={images[8]} link="/" title="Crates" paragraph=""/>
+        <Article imgUrl={images[9]} link="/" title="Village Island" paragraph=""/>
+        <Article imgUrl={images[10]} link="/" title="Dead God Island" paragraph=""/>
+        <Article imgUrl={images[11]} link="/" title="Lobue" paragraph=""/>
+        <Article imgUrl={images[12]} link="/" title="Old lady" paragraph=""/>
+        <Article imgUrl={images[13]} link="/" title="Middle aged man" paragraph=""/>
+        <Article imgUrl={images[14]} link="/" title="Child" paragraph=""/>
+        <Article imgUrl={images[15]} link="/" title="Bob" paragraph=""/>
+        <Article imgUrl={images[16]} link="/" title="Ship" paragraph=""/>
+        <Article imgUrl={images[17]} link="/" title="Raft" paragraph=""/>
+        <Article imgUrl={images[18]} link="/" title="Stone Modular Components" paragraph=""/>
+        <Article imgUrl={images[19]} link="/" title="Wooden Modular Components" paragraph=""/>
+        <Article imgUrl={images[20]} link="/" title="Wooden Modular Components" paragraph=""/>
+        <Article imgUrl={images[21]} link="/" title="Palm tree" paragraph=""/>
+        <Article imgUrl={images[22]} link="/" title="Big Tree" paragraph=""/>
+        <Article imgUrl={images[23]} link="/" title="Oak Trees" paragraph=""/>
+        <Article imgUrl={images[24]} link="/" title="Red Mushrooms" paragraph=""/>
+        <Article imgUrl={images[25]} link="/" title="Brown Mushrooms" paragraph=""/>
+        <Article imgUrl={images[26]} link="/" title="Barrel" paragraph=""/>
+
       </div>
     </div>
    ) 
